001d9deda806fbaf373a2303eb6e93ff0a193aa9
unknown <durza007@gmail.com>
2013-03-05 19:37:50 +0100

    cleanup in selectplayerfragment

--- src/com/minder/app/tf2backpack/frontend/SelectPlayerFragment.java
+++ src/com/minder/app/tf2backpack/frontend/SelectPlayerFragment.java
@@ -7,12 +7,10 @@ import java.net.UnknownHostException;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
@@ -190,8 +188,6 @@ public class SelectPlayerFragment extends Fragment {
 	public static interface OnSearchClickedListener {
 		public void onSearchClicked(String searchQuery);
 	}
-
-	private final static int COMMUNITY_ID_TUTORIAL = 1;
 
 	private WeakReference<OnPlayerSelectedListener> playerSelectedListener;
 	private WeakReference<OnSearchClickedListener> searchClickedListener;
